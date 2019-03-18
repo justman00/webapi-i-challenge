fda11d64a9c47ab8c75f5a5bec6d59edeba2b7b2
Vlad <vovaturcan4@gmail.com>
2019-03-18 21:39:52 +0200

    did the get route, get by id and post route

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+// GET methods
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." })
+    );
+});
+
+server.get("/api/users/:id", (req, res) => {
+  console.log(req.params.id);
+  db.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." })
+    );
+});
+
+/////////////////////////////////////////////////////////
+
+// CUD Methods
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  db.insert(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the user to the database"
+        })
+    );
+});
+
+server.listen(5000, () => {
+  console.log("It's woring");
+});
